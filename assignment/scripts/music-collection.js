888a497c8a4ceb26994069c708c56dac0756b97e
Philip Krynicki <philipkrynicki@yahoo.com>
2021-05-08 19:34:47 -0500

    Reinforcement needed changes attempted on this assignment

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -6,16 +6,16 @@ let collection = [];
 
 // Add a function named addToCollection
 
-function addToCollection (title, artist, yearPublished, tracks) {
+function addToCollection (title, artist, yearPublished) {
   const record = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
-    tracks: tracks
   }   //  end of record album designation
   collection.push(record);
   return record;
 }  //  end of addToCollection function
+
 
 //  Test the addToCollection function by adding six albums to your Collection
 console.log('Adding first album to collection', addToCollection('Check Your Head', 'Beastie Boys', '1992'));
@@ -29,11 +29,11 @@ console.log('Adding sixth album to collection', addToCollection('Diary of a Madm
 console.log(collection);
 //  console.log the collection array
 //  First commit added here
-let artist = ['White Lion', 'Beastie Boys', 'Quiet Riot', 'Ozzy Osbourne'];
+//  Artist array deleted on this line
 //  Add a function named showCollection
 function showCollection (array) {
-  console.log('My collection contains ${array.length} records.');
-//  Something is wrong on line 35
+  console.log(`My collection contains ${array.length} records.`);
+//  Backticks added to Line 35
   for (let items of array) {
     console.log(`${items.title} by ${items.artist} published ${items.yearPublished}:`);
   }
@@ -44,15 +44,22 @@ console.log('I would like to see what we have in the collection');
 showCollection(collection);
 //  Test the showCollection function
 
+
+
 // Add a function named findByArtist
 function findByArtist (artist){
   let emptyArray = [];
-    for (let i=0; i<collection.lengh; i++){
-      if (artist = collection.title){
+    for (let i=0; i<collection.length; i++){
+      if (artist === collection[i].artist){
+        return collection[i];
+//  Attemted to use forEach at this point but wasn't successful.  I'm not sure what to add to show all the albums from an artists.  Beastie Boys have two albums, but they don't show up in the console.log
     }
   }
 }
-// Not completely finished with this section of the work
+// Correction made to spelling of length on Line 50
 
 // Test the findByArtist function
-console.log( findByArtist( 'Beastie Boys', artist));
+
+console.log( findByArtist( 'Beastie Boys' ));
+console.log( findByArtist('White Lion'));
+console.log( 'Pantera should be undefined:', findByArtist('Pantera'));
